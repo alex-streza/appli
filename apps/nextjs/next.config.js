c92d1bec0a552e9a0bbd67a762040e9bdb0c9d92
Turbobot <turbobot@vercel.com>
2024-03-12 18:03:20 +0200

    feat(create-turbo): create https://github.com/t3-oss/create-t3-turbo

---
+++ apps/nextjs/next.config.js
@@ -0,0 +1,28 @@
+import { fileURLToPath } from "url";
+import _jiti from "jiti";
+
+const jiti = _jiti(fileURLToPath(import.meta.url));
+
+// Import env files to validate at build time. Use jiti so we can load .ts files in here.
+jiti("./src/env");
+jiti("@acme/auth/env");
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: [
+    "@acme/api",
+    "@acme/auth",
+    "@acme/db",
+    "@acme/ui",
+    "@acme/validators",
+  ],
+
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
+
+export default config;
