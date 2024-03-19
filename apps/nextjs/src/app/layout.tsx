2477fcd6323fe09a3e1e6d4bc935ecfa37b659b2
Syed Muzamil <smmhd121@gmail.com>
2024-03-19 11:48:16 +0530

    Added more .env varaibles and change the title
    Changed the meta title and description
    
    Add required env variable

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -19,11 +19,11 @@ export const metadata: Metadata = {
       ? "https://turbo.t3.gg"
       : "http://localhost:3000",
   ),
-  title: "Create T3 Turbo",
-  description: "Simple monorepo with shared backend for web & mobile apps",
+  title: "Appli",
+  description: "Appli: Streamline tech job applications",
   openGraph: {
-    title: "Create T3 Turbo",
-    description: "Simple monorepo with shared backend for web & mobile apps",
+    title: "Appli",
+    description: "Appli: Streamline tech job applications",
     url: "https://create-t3-turbo.vercel.app",
     siteName: "Create T3 Turbo",
   },
